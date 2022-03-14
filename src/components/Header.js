d637f8c1a427beaee356360d609d6f8750ff5abe
Olugbenga Ajayi <gbejula@gmail.com>
2022-03-14 18:15:59 +0100

    toggle button to display close and add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button text='Add' color='green' onClick={onAdd} />
+      <Button
+        text={showAdd ? 'Close' : 'Add'}
+        color={showAdd ? 'red' : 'green'}
+        onClick={onAdd}
+      />
     </header>
   );
 };
